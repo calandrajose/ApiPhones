dfc37181c073ce2a4ee72bbb841c3b671dd7f442
Rodrigo <Leon>
2020-04-28 19:03:22 -0300

    basic structure

---
+++ src/test/java/com/phones/phones/PhonesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phones.phones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
